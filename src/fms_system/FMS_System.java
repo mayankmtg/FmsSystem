b78c72f58b083f1ea5a615b62b34747be4344027
mayankmtg <mayank15056@iiitd.ac.in>
2016-11-05 18:35:47 +0530

    first

---
+++ src/fms_system/FMS_System.java
@@ -0,0 +1,17 @@
+package fms_system;
+
+/**
+ *
+ * @author mayank
+ * @author amit
+ */
+public class FMS_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
