8640e8a9dad748d21d0c1d7a403c5859dd4aec51
mayankmtg <mayank15056@iiitd.ac.in>
2016-11-08 19:56:00 +0530

    Logistic_Approval_Request

---
+++ src/fms_system/Logistic_Approval_Request.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fms_system;
+
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+/**
+ *
+ * @author mayank
+ */
+public class Logistic_Approval_Request extends JFrame{
+    int width=800;
+    int height=500;
+    public Logistic_Approval_Request(){
+        
+        super("Task Assigner");
+        setSize(width,height);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(null);
+        
+    }
+    
+}
