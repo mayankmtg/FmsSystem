abcf76a8d4d856c7ee9fc45bb480a1ed8bb1a2fa
Amit <amit15013@iiitd.ac.in>
2016-11-21 23:56:17 +0530

    leave gui

--- src/fms_system/Leave.java
+++ src/fms_system/Leave.java
@@ -6,13 +6,15 @@
 package fms_system;
 
 import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JLabel;
-import javax.swing.JPasswordField;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
@@ -22,8 +24,8 @@ import javax.swing.SwingConstants;
  * @author amit
  */
 public class Leave extends JFrame{
-    int width=800;
-    int height=500;
+    int width=1000;
+    int height=700;
     public Leave(){
         
         super("Leave Application");
@@ -38,38 +40,36 @@ public class Leave extends JFrame{
         add(title);
         
         JLabel Texts[]=new JLabel[4];
-        Texts[0]=new JLabel("To Whome:");
+        Texts[0]=new JLabel("To Whom:");
         Texts[1]=new JLabel("Reason:");
         Texts[2]=new JLabel("Start Date:");
         Texts[3]=new JLabel("End Date:");
-        
-        for(int i=0;i<4;i++){
-            Texts[i].setLocation(400, 90+50*i);
-            Texts[i].setSize(200,20);
-            add(Texts[i]);
-        }
-        ///////////////////////////////////////
+        String[] box={"Admin","Department_Supervisor"};
+        JComboBox whom=new JComboBox(box);
         JTextField[] Fields=new JTextField[4];
-        
-        for(int i=0;i<4;i++){
-            if(i==0) { 
-                Fields[0].setEnabled(false);
-                continue;
-            }
+        JPanel leave=new JPanel(new GridLayout(4,2,30,30));
+        for(int i=0;i<4;i++)
+        {
             Fields[i]=new JTextField();
-            Fields[i].setLocation(400, 90+50*i);
-            Fields[i].setSize(200,20);
-            add(Fields[i]);
+            leave.add(Texts[i]);
+            if(i!=0)
+                leave.add(Fields[i]);
+            else{
+                leave.add(whom);
+            }
         }
+        leave.setLocation(300,150);
+        leave.setSize(400,400);
+        add(leave);
+        
+        
         JButton sub=new JButton("Apply");
-        sub.setLocation(350, 300);
+        sub.setLocation(300,600);
         sub.setSize(200,40);
         sub.setForeground(Color.white);
         sub.setBackground(Color.red);
         sub.addActionListener(new ActionListener(){
-            @Override
             public void actionPerformed(ActionEvent ae) {
-                //code after leave application
             }
         });
         add(sub);
