822b69e2dfe4d98104e68eea81641deac6a6e51b
Amit <amit15013@iiitd.ac.in>
2016-11-22 23:43:55 +0530

    leave form complete

--- src/fms_system/Leave.java
+++ src/fms_system/Leave.java
@@ -9,11 +9,16 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -26,6 +31,7 @@ import javax.swing.SwingConstants;
 public class Leave extends JFrame{
     int width=1000;
     int height=700;
+    Staff staff_obj;
     public Leave(){
         
         super("Leave Application");
@@ -44,7 +50,7 @@ public class Leave extends JFrame{
         Texts[1]=new JLabel("Reason:");
         Texts[2]=new JLabel("Start Date:");
         Texts[3]=new JLabel("End Date:");
-        String[] box={"Admin","Department_Supervisor"};
+        String[] box={"Admin","Supervisor"};
         JComboBox whom=new JComboBox(box);
         JTextField[] Fields=new JTextField[4];
         JPanel leave=new JPanel(new GridLayout(4,2,30,30));
@@ -69,7 +75,28 @@ public class Leave extends JFrame{
         sub.setBackground(Color.green);
         sub.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent ae) {
-                
+                Fields[0].setText(whom.getSelectedItem().toString());
+                try{
+                        PrintWriter writer = new PrintWriter(new FileWriter("database/leave.csv",true));
+                        String userData="";
+                        for(int i=0;i<3;i++){
+                            if(Fields[0].getText().equals("Supervisor") && i==0){
+                                System.out.print(Fields[0].getText());
+                                userData+=Login.getCurrentDept()+",";
+                            }
+                            else
+                                userData+=Fields[i].getText() +",";
+                        }
+                        userData+=Fields[3].getText();
+                        writer.println(userData);
+                        writer.close();
+                        } catch (Exception e) {
+                    }
+                JOptionPane.showMessageDialog (null,"Hey! You have been succssfully sent a requset.","Thank You", JOptionPane.INFORMATION_MESSAGE);
+                for(int i=1;i<4;i++)
+                {
+                    Fields[i].setText("");
+                }
             }
         });
         add(sub);
@@ -80,6 +107,15 @@ public class Leave extends JFrame{
         back.setForeground(Color.white);
         back.setBackground(Color.red);
         add(back);
+        back.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                staff_obj=new Staff();
+                staff_obj.setVisible(true);
+            }
+            
+        });
     }
 }
 
